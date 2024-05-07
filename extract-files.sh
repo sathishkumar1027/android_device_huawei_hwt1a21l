53b929c818879ad122312083fe22219facf9be3b
sathishkumar1027 <sathishkumar1027@gmail.com>
2024-05-07 15:11:09 +0300

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,9 +18,41 @@
 
 set -e
 
-# Required!
-export DEVICE=cherry
-export DEVICE_COMMON=msm8916-common
-export VENDOR=huawei
+DEVICE=hwt1a21l
+VENDOR=huawei
 
-./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh
